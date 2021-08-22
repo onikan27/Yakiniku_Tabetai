ab934b40c37f2c5d193e328bf71c9513d994df58
onikan27 <ude0721juji@icloud.com>
2021-08-22 19:55:51 +0900

    feat: add logic

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module line_search_restaurant
 
 go 1.16
 
-require github.com/line/line-bot-sdk-go/v7 v7.10.0 // indirect
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/line/line-bot-sdk-go/v7 v7.10.0
+)
--- main.go
+++ main.go
@@ -1,30 +1,60 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"unicode/utf8"
 
+	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
+type responseType struct {
+	Results results `json:"results"`
+}
+
+type results struct {
+	Shop []shop `json:"shop"`
+}
+
+type shop struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+	Photo   photo  `json:"photo"`
+	URLS    urls   `json:"urls"`
+}
+
+type photo struct {
+	Mobile mobile `json:"mobile"`
+}
+
+type mobile struct {
+	L string `json:"l"`
+}
+
+type urls struct {
+	PC string `json:"pc"`
+}
+
 func main() {
-	http.HandleFunc("/", confirmHandler)
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	}
 	http.HandleFunc("/call", callHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func confirmHandler(w http.ResponseWriter, r *http.Request) {
-	msg := "動作確認"
-	fmt.Fprintf(w, msg)
-}
-
 func callHandler(w http.ResponseWriter, r *http.Request) {
 	bot, err := linebot.New(
-		"シークレットキー",
-		"アクセスキー",
+		os.Getenv("LINE_SECRET_KEY"),
+		os.Getenv("LINE_ACCESS_KEY"),
 	)
 	if err != nil {
 		log.Fatal(err)
@@ -41,29 +71,80 @@ func callHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
-			switch message := event.Message.(type) {
-			case *linebot.TextMessage:
-				replyMessage := message.Text
-				_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
-				if err != nil {
+			switch event.Message.(type) {
+			case *linebot.LocationMessage:
+				sendYakinikuRestaurantInfo(bot, event)
+			default:
+				replyMessage := "位置情報を送ってください。"
+				if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
 					log.Print(err)
 				}
-			case *linebot.LocationMessage:
-				sendRestaurantInfo(bot, event)
 			}
 		}
 	}
 }
 
-func sendRestaurantInfo(bot *linebot.Client, event *linebot.Event) {
-	msg := event.Message.(*linebot.LocationMessage)
+func sendYakinikuRestaurantInfo(bot *linebot.Client, event *linebot.Event) {
+	message := event.Message.(*linebot.LocationMessage)
 
-	latitude := strconv.FormatFloat(msg.Latitude, 'f', 2, 64)
-	longitude := strconv.FormatFloat(msg.Longitude, 'f', 2, 64)
+	latitude := strconv.FormatFloat(message.Latitude, 'f', 2, 64)
+	longitude := strconv.FormatFloat(message.Longitude, 'f', 2, 64)
 
-	replyMsg := fmt.Sprintf("緯度：%s\n経度：%s", latitude, longitude)
-	_, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMsg)).Do()
-	if err != nil {
+	replyMsg := getYakinikuRestaurantInfo(latitude, longitude)
+
+	res := linebot.NewTemplateMessage(
+		"焼肉一覧",
+		linebot.NewCarouselTemplate(replyMsg...).WithImageOptions("rectangle", "cover"),
+	)
+
+	if _, err := bot.ReplyMessage(event.ReplyToken, res).Do(); err != nil {
 		log.Print(err)
 	}
+}
+
+func getYakinikuRestaurantInfo(latitude string, longitude string) []*linebot.CarouselColumn {
+	apikey := os.Getenv("TABELOG_API_KEY")
+	searchWord := "焼肉"
+	endpoint := "https://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
+
+	url := fmt.Sprintf(
+		"%s?format=json&key=%s&lat=%s&lng=%s&keyword=%s",
+		endpoint, apikey, latitude, longitude, searchWord)
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data responseType
+
+	if err := json.Unmarshal(body, &data); err != nil {
+		log.Fatal(err)
+	}
+
+	var carouselList []*linebot.CarouselColumn
+	for _, shop := range data.Results.Shop {
+		address := shop.Address
+		if 60 < utf8.RuneCountInString(address) {
+			address = string([]rune(address)[:60])
+		}
+
+		carouselItem := linebot.NewCarouselColumn(
+			shop.Photo.Mobile.L,
+			shop.Name,
+			address,
+			linebot.NewURIAction("詳しく見てみる", shop.URLS.PC),
+		).WithImageOptions("#ffffff")
+		carouselList = append(carouselList, carouselItem)
+	}
+	return carouselList
 }
